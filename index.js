b4a95bdc26a05fdf266c94eb025e95831e61e3a7
nodjinar Tassinal <nodjinarta@gmail.com>
2023-05-05 17:07:19 +0200

    premier commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("http://localhost:5678/api/works");
+const travaux = await reponse.json();
+console.log(travaux);
+
+
+// Creation des balises
+function genenertravaux (travaux){ 
+for (let i = 0; i < travaux.length; i++) {
+    const figure = travaux[i];
+    const divGalery = document.querySelector(".gallery");
+const sectionfigure = document.createElement('figure');
+
+const imageElement = document.createElement("img");
+imageElement.src = figure.imageUrl;
+const nomElement = document.createElement("figcaption");
+nomElement.innerText = figure.title;
+
+
+
+divGalery.appendChild(sectionfigure);
+sectionfigure.appendChild(imageElement);
+sectionfigure.appendChild(nomElement);
+
+}
+}
+genenertravaux(travaux);
+
+
+/*const boutonEffacer = document.querySelector('#btn-4');
+boutonEffacer.addEventListener('click',function(){
+    document.querySelector('.gallery').innerHTML= "";
+})*/
+
+
+const boutonTous = document.querySelector('#btn-1');
+boutonTous.addEventListener('click',function(){
+    const tout =travaux.filter(function(tous){
+        return tous
+    });
+    console.log(tout);
+    document.querySelector('.gallery').innerHTML="";
+    genenertravaux(tout);
+});
+
+const boutonObject = document.querySelector('#btn-2');
+boutonObject.addEventListener('click',function(){
+    const objects =travaux.filter(function(tous){
+        return tous.categoryId===1;
+    });
+    console.log(objects);
+    document.querySelector('.gallery').innerHTML="";
+    genenertravaux(objects);
+});
+
+
+const boutonAppartement = document.querySelector('#btn-3');
+boutonAppartement.addEventListener('click',function(){
+    const appartements=travaux.filter(function(tous){
+        return tous.categoryId===2;
+    });
+    console.log(appartements);
+    document.querySelector('.gallery').innerHTML="";
+    genenertravaux(appartements);
+});
+
+const boutonHotel = document.querySelector('#btn-4');
+boutonHotel.addEventListener('click',function(){
+    const hotels=travaux.filter(function(tous){
+        return tous.categoryId===3;
+    });
+    console.log(hotels);
+    document.querySelector('.gallery').innerHTML="";
+    genenertravaux(hotels);
+});
+
+
+
+
+
+
+
